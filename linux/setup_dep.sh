4190ed7b5adfe9d1d8f6e11077a3997d5e4b4440
mai0313 <mai@mai0313.com>
2023-08-19 07:02:24 +0000

    init

--- linux/setup_dep.sh
+++ linux/setup_dep.sh
@@ -3,7 +3,7 @@
 # 安裝依賴
 if [ -f "requirements.txt" ]; then
     echo "安裝依賴..."
-    pip3 install -r requirements.txt
+    pip install -r requirements.txt
 else
     echo "未找到requirements.txt文件，跳過依賴安裝"
 fi
